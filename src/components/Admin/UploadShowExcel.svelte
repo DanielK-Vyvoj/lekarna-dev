1fc8318a51c9c92846988fd6357aa3b9e55ff08e
Daniel Krčil <daniel.kvyvoj@gmail.com>
2023-06-02 15:25:09 +0000

    ver 1.0

--- src/components/Admin/UploadShowExcel.svelte
+++ src/components/Admin/UploadShowExcel.svelte
@@ -1,104 +1,121 @@
 <script lang="ts">
-    import { onMount } from "svelte";
-    import { read, utils } from "xlsx";
-    import type { Worksheet } from "xlsx";
-    import type { FirebaseRef, DatabaseReference } from "firebase/database";
-    import { db } from "../../firebase/Firebase"; // Import instance databáze z vašeho `firebase.js` souboru
   
-    let selectedFile: File | undefined;
-    let excelData: any[] = [];
+  import { onMount } from "svelte";
+  import { read, utils } from "xlsx";
+  import type { Worksheet } from "xlsx";
+  import { getDatabase, ref, push } from "firebase/database";
+  import Swal from "sweetalert2";
+  import { app } from "../../firebase/Firebase";
+    
   
-    function handleFileUpload(event: InputEvent) {
-      const fileInput = event.target as HTMLInputElement;
-      if (fileInput.files && fileInput.files.length > 0) {
-        selectedFile = fileInput.files[0];
-        const reader = new FileReader();
-  
-        reader.onload = (e) => {
-          const data = new Uint8Array(e.target.result as ArrayBuffer);
-          const workbook = read(data, { type: "array" });
-          const sheetName = workbook.SheetNames[0]; // Předpokládáme, že chcete načíst první list
-  
-          const worksheet = workbook.Sheets[sheetName] as Worksheet;
-          excelData = utils.sheet_to_json(worksheet, { header: 1 });
-  
-          console.log(excelData); // Vypsání dat z Excelu v konzoli
-        };
-  
-        reader.readAsArrayBuffer(selectedFile);
+  let selectedFile: File | undefined;
+  let excelData: any[] = [];
+  let selectedCells: { row: number; col: number; value: string }[] = [];
+
+  function handleFileUpload(event: Event) {
+    const fileInput = event.target as HTMLInputElement;
+    if (fileInput.files && fileInput.files.length > 0) {
+      selectedFile = fileInput.files[0];
+      const reader = new FileReader();
+
+      reader.onload = (e: ProgressEvent<FileReader>) => {
+        const data = new Uint8Array(e.target!.result as ArrayBuffer);
+        const workbook = read(data, { type: "array" });
+        const sheetName = workbook.SheetNames[0];
+        const worksheet = workbook.Sheets[sheetName] as Worksheet;
+        excelData = utils.sheet_to_json(worksheet, { header: 1 });
+
+        console.log(excelData);
+      };
+
+      reader.readAsArrayBuffer(selectedFile);
+    }
+  }
+
+  async function handleSaveData() {
+    if (selectedCells.length > 0) {
+      try {
+        const newRef = ref(getDatabase(app), "excelData");
+        const snapshot = await push(newRef, selectedCells);
+        console.log("Data byla uložena do databáze:", snapshot.key);
+      } catch (error) {
+        console.error("Chyba při ukládání dat:", error);
       }
     }
-  
-    function handleSaveData() {
-      if (excelData.length > 0) {
-        const newRef: FirebaseRef = db.ref("excelData").push();
-        newRef
-          .set(excelData)
-          .then(() => {
-            console.log("Data byla uložena do databáze.");
-          })
-          .catch((error) => {
-            console.error("Chyba při ukládání dat:", error);
-          });
-      }
+  }
+
+  function handleSelectData(row: number, col: number, value: string) {
+    const cellIndex = selectedCells.findIndex(
+      (cell) => cell.row === row && cell.col === col
+    );
+
+    if (cellIndex > -1) {
+      selectedCells.splice(cellIndex, 1);
+    } else {
+      selectedCells.push({ row, col, value });
     }
-  
-    onMount(() => {
-      // Reset selectedFile a excelData při načítání komponenty
-      selectedFile = undefined;
-      excelData = [];
-    });
-  </script>
-  
-  <main class="p-6">
-    <h1 class="text-2xl font-bold mb-4">Nahrát a zobrazit Excel soubor</h1>
-  
-    <div>
-      <label for="excelFile" class="block text-sm font-medium text-gray-700 mb-1">
-        Vyberte Excel soubor:
-      </label>
-      <input
-        type="file"
-        id="excelFile"
-        accept=".xlsx"
-        class="py-2 px-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm"
-        on:change={handleFileUpload}
-      />
-    </div>
-  
-    {#if excelData.length > 0}
-      <h2 class="text-xl font-bold mt-6 mb-2">Zobrazená data:</h2>
-  
-      <table class="min-w-full border border-gray-300">
-        <thead>
+  }
+
+  onMount(() => {
+    selectedFile = undefined;
+    excelData = [];
+    selectedCells = [];
+  });
+</script>
+
+<main class="p-6">
+  <h1 class="text-2xl font-bold mb-4">Nahrát a zobrazit Excel soubor</h1>
+
+  <div>
+    <label for="excelFile" class="block text-sm font-medium text-gray-700 mb-1">
+      Vyberte Excel soubor:
+    </label>
+    <input
+      type="file"
+      id="excelFile"
+      accept=".xlsx"
+      class="py-2 px-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm"
+      on:change={handleFileUpload}
+    />
+  </div>
+
+  {#if excelData.length > 0}
+    <h2 class="text-xl font-bold mt-6 mb-2">Zobrazená data:</h2>
+
+    <table class="min-w-full border border-gray-300">
+      <thead>
+        <tr>
+          {#each excelData[0] as headerItem}
+            <th class="py-2 px-4 border-b border-gray-300">{headerItem}</th>
+          {/each}
+        </tr>
+      </thead>
+      <tbody>
+        {#each excelData.slice(1) as row, rowIndex}
           <tr>
-            {#each excelData[0] as headerItem}
-              <th class="py-2 px-4 border-b border-gray-300">{headerItem}</th>
+            {#each row as cell, colIndex}
+              <td
+                class="py-2 px-4 border-b border-gray-300"
+                class:selected={selectedCells.find(
+                  (selectedCell) =>
+                    selectedCell.row === rowIndex + 1 &&
+                    selectedCell.col === colIndex
+                )}
+                on:click={() => handleSelectData(rowIndex + 1, colIndex, cell)}
+                key={`${rowIndex}-${colIndex}`}
+              >{cell}</td>
             {/each}
           </tr>
-        </thead>
-        <tbody>
-          {#each excelData.slice(1) as row}
-            <tr>
-              {#each row as cell}
-                <td class="py-2 px-4 border-b border-gray-300">{cell}</td>
-              {/each}
-            </tr>
-          {/each}
-        </tbody>
-      </table>
-  
-      <button
-        type="button"
-        class="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-        on:click={handleSaveData}
-      >
-        Uložit data do databáze
-      </button>
-    {/if}
-  </main>
-  
-  <style>
-    /* Přidejte vlastní styly pro komponentu */
-  </style>
-  
+        {/each}
+      </tbody>
+    </table>
+
+    <button
+      type="button"
+      class="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      on:click={handleSaveData}
+    >
+      Uložit data do databáze
+    </button>
+  {/if}
+</main>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,58 @@
-<h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+<script lang="ts">
+    import { onMount } from 'svelte';
+  
+    let cards = [
+      { title: 'Fit Lékárna', location: 'Olomouc' },
+      // Další karty zde
+    ];
+  
+    onMount(() => {
+      // Volání funkce pro načtení dat karet nebo jiné inicializační úkoly
+    });
+  </script>
   
   <style lang="postcss">
-    :global(html) {
-      background-color: theme(colors.gray.100);
+    .card-container {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+    }
+  
+    .card {
+      width: 200px;
+      height: 200px;
+      margin-bottom: 10px;
+      background-color: #4CAF50;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      color: white;
+      animation: pulse 2s infinite;
+    }
+  
+    @keyframes pulse {
+      0% {
+        transform: scale(1);
+      }
+      50% {
+        transform: scale(1.1);
+      }
+      100% {
+        transform: scale(1);
+      }
     }
   </style>
+  
+  <main>
+    <div class="flex justify-center">
+      <div class="card-container">
+        {#each cards as card}
+          <div class="card">
+            <h3>{card.title}</h3>
+            <p>Město: {card.location}</p>
+          </div>
+        {/each}
+      </div>
+    </div>
+  </main>
+  
